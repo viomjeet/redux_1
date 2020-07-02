7699dd38441731ef6084c543f25462234531958f
vikas kumar <viomjeet@gmail.com>
2020-07-02 22:00:09 +0530

    Updates

---
+++ precache-manifest.43c6ff8695467a985f8b034834840d32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51df0f2287eeaa04d0e8901b603ac3a3",
+    "url": "/redux_1/index.html"
+  },
+  {
+    "revision": "1e860bce54600d724c8d",
+    "url": "/redux_1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d807a2201d50f152f79b",
+    "url": "/redux_1/static/css/main.45992b7e.chunk.css"
+  },
+  {
+    "revision": "1e860bce54600d724c8d",
+    "url": "/redux_1/static/js/2.f897dbcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux_1/static/js/2.f897dbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d807a2201d50f152f79b",
+    "url": "/redux_1/static/js/main.e2a96a74.chunk.js"
+  },
+  {
+    "revision": "a3b6169ccd3a2cb8dd76",
+    "url": "/redux_1/static/js/runtime-main.caf14c42.js"
+  }
+]);
